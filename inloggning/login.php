4b77a2fa1e3db2b41e3b474f326491908584a62e
mranstrand <michael.ranstrand@gmail.com>
2015-03-24 07:47:28 +0100

    Skapar order vid inlogg

--- inloggning/login.php
+++ inloggning/login.php
@@ -13,7 +13,7 @@ $password = $_POST["password"];
 
 //Variabler för databaskoppling
 $dbhost     = "localhost";
-$dbname     = "webbshop";
+$dbname     = "the_great_shop";
 $dbuser     = "root";
 $dbpass     = "";
 
@@ -24,7 +24,7 @@ $DBH = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 
 // Förbered databasfråga med placeholders (markerade med : i början)
-$STH = $DBH->prepare("SELECT * FROM tbl_kunder WHERE kundnamn = :username AND password = :password");
+$STH = $DBH->prepare("SELECT * FROM tbl_kund WHERE login = :username AND passwd = :password");
 
 //Ersätt placeholders med värden från variabler
 $STH->bindParam(':username', $username);
@@ -33,16 +33,29 @@ $STH->bindParam(':password', $password);
 //Utför frågan
 $STH->execute();
 
-//Stänger databaskopplingen
-$DBH = null;
+
 
 //Undersök om nåfon användare matchar frågan
 if($row = $STH->fetch()){
 
-    $_SESSION["kundnamn"] = $row["kundnamn"];
-    $_SESSION["kundid"] = $row["kundid"];
+    $_SESSION["kundnamn"] = $row["login"];
+    $_SESSION["kundid"] = $row["ID"];
     print_r($_SESSION);
     //header("Location: index.php");
+
+    $STH = $DBH->prepare("INSERT INTO tbl_order (kundID, status, regdat) value (:kundID, :status, CURRENT_DATE())");
+
+    $status = "varukorg";
+    //Ersätt placeholders med värden från variabler
+    $STH->bindParam(':kundID', $_SESSION["kundid"]);
+    $STH->bindParam(':status', $status);
+
+    //Utför frågan
+    $STH->execute();
+
+
 }
 
+//Stänger databaskopplingen
+$DBH = null;
 ?>
